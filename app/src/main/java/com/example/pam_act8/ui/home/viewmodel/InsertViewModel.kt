0da418f508f7a7a83adf3420aca30412f229ea68
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2023-12-28 16:14:26 +0700

    add dataclass at Insert UI

---
+++ app/src/main/java/com/example/pam_act8/ui/home/viewmodel/InsertViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.pam_act8.ui.home.viewmodel
+
+data class InsertUiEvent(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val nohp: String = ""
+)
